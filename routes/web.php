6e703089fddf70e7387a5b333d2c7f53ec313db2
darelaj <darelfahrezi@gmail.com>
2023-12-14 15:02:17 +0700

    membuat route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,5 +28,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/pemesanan', [TransactionController::class, 'create'])->middleware(['auth', 'verified'])->name('order');
+Route::post('/pemesananStore', [TransactionController::class, 'store'])->middleware(['auth', 'verified'])->('orderStore');
+Route::get('/status', [TransactionController::class, 'index'])->middleware(['auth', 'verified'])->name('status');
+
 
 require __DIR__.'/auth.php';
