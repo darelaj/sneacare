1afe253ad1eb86cab560075414107dae85371cd6
FarisBagas <Neocrmencer@gmail.com>
2024-01-05 08:05:58 +0700

    feat tampilan dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,90 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    
+    <!-- about us -->
+    <section class="flex items-center py-10 bg-stone-100 xl:h-screen font-poppins  ">
+        <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+            <div class="flex flex-wrap ">
+                <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+                    <img src="https://cdn.discordapp.com/attachments/698554518278897664/1192578683832119357/Group_1.png" alt=""
+                    class="w-1/2 px-4 mb-10  ">
+                </div>
+                <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0 ">
+                    <div class="relative">
+                        <h1
+                            class="absolute -top-20   left-0 text-[20px] lg:text-[100px] text-[#C4A7A7] font-bold  dark:text-[#020202] opacity-15 md:block hidden">
+                            About Us
+                        </h1>
+                        <h1 class="pl-2 text-3xl font-bold border-l-8 border-blue-400 md:text-5xl dark:text-black">
+                            Welcome to Sneacare
+                        </h1>
+                    </div>
+                    <p class="mt-6 mb-10 text-base leading-7 text-gray-500 dark:text-gray-400">
+                    “Sneacare” adalah sebuah aplikasi website yang menyediakan jasa cuci sepatu secara online. 
+                    Website ini akan menyediakan jasa pencucian sepatu untuk di sekitar Telkom University. 
+                    Calon Customer yang dituju adalah masyarakat sekitar Telkom University yang sedang membutuhkan jasa perawatan sepatu, 
+                    website ini juga ditujukan untuk para penyedia jasa treatment sepatu untuk mendaftarkan usahanya di website ini agar 
+                    lebih mudah untuk mendapatkan customer.
+                    </p>
+                   
+            </div>
+        </div>
+    </section>
+
+    <!-- Repaint -->
+    <div class=" justify-center mt-5">
+        <div class="w-[1000px] h-[300px] shadow-xl mx-auto bg-[#0097FF] rounded-3xl relative">
+            <h1 class="text-5xl ml-9 pt-4" style="font-family: 'Inter', sans-serif; font-family: 'Poppins', sans-serif;"> REPAINT </h1>
+            <div class="w-[400px] h-[300px] shadow-xl mx-auto bg-[#d9d9d9] rounded-3xl absolute inset-y-0 right-0">
+                <img src="https://cdn.discordapp.com/attachments/698554518278897664/1192162347251417280/Jordan1-removebg-preview_1.png" alt=""
+                class = "mt-10">
+            </div>
+
+            <button class="bg-white rounded-3xl w-[300px] h-[70px] mt-[150px] ml-10 text-2xl" style="font-family: 'Inter';">
+                LEARN MORE
+            </button>
+        </div>
+    </div>
+
+    <!-- Repair -->
+    <div class=" justify-center mt-36">
+        <div class="w-[1000px] h-[300px] shadow-xl mx-auto bg-[#0097FF] rounded-3xl relative">
+            <h1 class="text-5xl ml-9 pt-4" style="font-family: 'Inter', sans-serif; font-family: 'Poppins', sans-serif;"> Repair </h1>
+            <div class="w-[400px] h-[300px] shadow-xl mx-auto bg-[#d9d9d9] rounded-3xl absolute inset-y-0 right-0">
+                <img src="https://cdn.discordapp.com/attachments/698554518278897664/1192609182982099014/Yeezy-removebg-preview_2.png" alt=""
+                class = "mt-10">
+            </div>
+
+            <button class="bg-white rounded-3xl w-[300px] h-[70px] mt-[150px] ml-10 text-2xl" style="font-family: 'Inter';">
+                LEARN MORE
+            </button>
+        </div>
+    </div>
+
+    <!-- Unyellowing -->
+    <div class=" justify-center mt-36">
+        <div class="w-[1000px] h-[300px] shadow-xl mx-auto bg-[#0097FF] rounded-3xl relative">
+            <h1 class="text-5xl ml-9 pt-4" style="font-family: 'Inter', sans-serif; font-family: 'Poppins', sans-serif;"> Unyellowing </h1>
+            <div class="w-[400px] h-[300px] shadow-xl mx-auto bg-[#d9d9d9] rounded-3xl absolute inset-y-0 right-0">
+                <img src="https://cdn.discordapp.com/attachments/698554518278897664/1192609261965037628/Shoes1_1.png" alt=""
+                class = "mt-10 w-[400px] h-[300]">
+            </div>
+
+            <button class="bg-white rounded-3xl w-[300px] h-[70px] mt-[150px] ml-10 text-2xl" style="font-family: 'Inter';">
+                LEARN MORE
+            </button>
+        </div>
+    </div>
+
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                
                 </div>
             </div>
         </div>
     </div>
+
+    
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,7 +2,7 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
+            <div class="flex ">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
@@ -14,6 +14,41 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                </div>
+
+                <!-- Home Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Home') }}
+                    </x-nav-link>
+                </div>
+
+                <!-- Support Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Order') }}
+                    </x-nav-link>
+                </div>
+
+                <!--  Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Status') }}
+                    </x-nav-link>
+                </div>
+
+                <!--  Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Support') }}
+                    </x-nav-link>
+                </div>
+
+                <!--  Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        {{ __('Contact') }}
                     </x-nav-link>
                 </div>
             </div>
